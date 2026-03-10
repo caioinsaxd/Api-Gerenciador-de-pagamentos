1d2f6b55d45b67cabdb952e20e938623d4a55f45
Caio Parize <caiosantanaparize2013@gmail.com>
2026-03-10 18:19:54 -0300

    feat: add codigo fonte da app+ validator

---
+++ start/validator.ts
@@ -0,0 +1,11 @@
+
+import { DateTime } from 'luxon'
+import { VineDate } from '@vinejs/vine'
+
+declare module '@vinejs/vine/types' {
+  interface VineGlobalTransforms {
+    date: DateTime
+  }
+}
+
+VineDate.transform((value) => DateTime.fromJSDate(value))
