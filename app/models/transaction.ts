6345537d95ab36b5c17255eafb11794e098aef5a
caioinsaxd <caiosantanaparize2015@gmail.com>
2026-03-11 16:58:27 -0300

    feat: add relação com o client no model da transaction

--- app/models/transaction.ts
+++ app/models/transaction.ts
@@ -1,5 +1,11 @@
 import { TransactionSchema } from '#database/schema'
+import { belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Client from './client.js'
 
 export default class Transaction extends TransactionSchema {
   static table = 'transactions'
+
+  @belongsTo(() => Client)
+  declare client: BelongsTo<typeof Client>
 }
